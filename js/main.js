48031c1e60ac61f91c9f0ba03c144cc2a5c0ade8
mariammaged <mariommaged51@gmail.com>
2017-08-17 12:15:45 +0200

    web-project
    c

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+$('#mission').fadeOut()
+$('#mission').fadeIn()
+$('#mission').fadeOut()
+$('#mission').fadeIn()
+$('#mission').fadeOut()
+$('#mission').fadeIn()
+$('.myfade').fadeOut()
+$('.myfade').fadeIn()
+$('.myfade').fadeOut()
+$('.myfade').fadeIn()
+$('.myfade').fadeOut()
+$('.myfade').fadeIn()
